1b903db3a537953125cc1398e4402812f7bfadd7
tinn266 <machadoagus02@gmail.com>
2026-08-22 12:24:15 -0300

    .

--- Tarea1/decisionTree.py
+++ Tarea1/decisionTree.py
@@ -33,9 +33,17 @@ def infoGain(table, attribute):
     return gain
 
 #id3
+def maxGainAttribute(table, attributes):
+    bestAttribute = None
+    bestGain = 0
+    for attribute in attributes:
+        gain = infoGain(table, attribute)
+        if gain > bestGain:
+            bestAttribute = attribute
+            bestGain = gain
+    return bestAttribute
 
 # (arbol_padre)->[(etiqueta1, arbol_hijo1), (etiqueta2, arbol_hijo2), ...]
-
 def getTree(attributes, table, min_info_gain):
     #si todos los resultados son iguales, retornar el resultado
     if entropy(table) == 0:
@@ -48,15 +56,8 @@ def getTree(attributes, table, min_info_gain):
     #si pasamos de acá, es que ya estamos en el else general
 
     #elegir atributo con mayor ganancia
-    bestAttribute = None
-    bestGain = 0
-    for attribute in attributes:
-        gain = infoGain(table, attribute)
-        if gain > min_info_gain:
-            if gain > bestGain:
-                bestAttribute = attribute
-                bestGain = gain
-
+    bestAttribute = maxGainAttribute(table, attributes)
+    
     # Si ningun atributo supera la ganancia minima, crear una hoja
     # con el resultado mayoritario.
     if bestAttribute is None:
@@ -79,3 +80,14 @@ def getTree(attributes, table, min_info_gain):
             )
     
     return tree.Tree(bestAttribute, children)
+
+def predict(tree, row):
+    if not tree.children:
+        return tree.value
+    else:
+        attribute_value = row[tree.value]
+        if attribute_value in tree.children:
+            return predict(tree.children[attribute_value], row)
+        else:
+            # Si el valor del atributo no está en los hijos, retornar None o un valor por defecto
+            return None
