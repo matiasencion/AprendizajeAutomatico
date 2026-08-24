ed28337abf26ccba00a960cb74df62732d46ef3c
tinn266 <machadoagus02@gmail.com>
2026-08-24 16:51:55 -0300

    cambios repo

---
+++ Tarea1/decisionTree/clasifier.py
@@ -0,0 +1,97 @@
+import math
+import tree
+
+class clasifier:
+    def __init__(self, min_info_gain=0):
+        self.tree = None
+        self.min_info_gain = min_info_gain
+
+    #entropia de la tabla
+    def entropy(self, table):
+        entropy = 0
+
+        possibleResults = table["result"].unique()
+        
+        for result in possibleResults:
+            aux = (table["result"] == result).sum() / table.shape[0]
+            aux = aux * math.log2(aux)
+            entropy += aux
+
+        return entropy * (-1)
+
+
+    #ganancia al particionar por un atributo
+    def infoGain(self, table, attribute):
+        ent = self.entropy(table)
+        uniqueValues = table[attribute].unique()
+        gain = 0
+        for value in uniqueValues:
+            aux = (table[attribute] == value).sum() / table[attribute].shape[0]
+            tableAux = table[table[attribute] == value] # TODO: checkear que si funciona asi
+            aux = aux * self.entropy(tableAux)
+            gain += aux
+        gain = ent - gain
+        return gain
+
+
+    def maxGainAttribute(self, table, attributes):
+        bestAttribute = None
+        bestGain = 0
+        for attribute in attributes:
+            gain = self.infoGain(table, attribute)
+            if gain > bestGain:
+                bestAttribute = attribute
+                bestGain = gain
+        return bestAttribute
+
+    # (arbol_padre)->[(etiqueta1, arbol_hijo1), (etiqueta2, arbol_hijo2), ...]
+    def getTree(self, attributes, table, min_info_gain):
+        #si todos los resultados son iguales, retornar el resultado
+        if self.entropy(table) == 0:
+            return tree.Tree(table["result"].unique()[0],{})  
+        
+        #si atributos es vacio
+        if len(attributes) == 0:
+            return tree.Tree(table["result"].mode()[0],{})
+        
+        #si pasamos de acá, es que ya estamos en el else general
+
+        #elegir atributo con mayor ganancia
+        bestAttribute = self.maxGainAttribute(table, attributes)
+        
+        # Si ningun atributo supera la ganancia minima, crear una hoja
+        # con el resultado mayoritario.
+        if bestAttribute is None:
+            return tree.Tree(table["result"].mode()[0], {})
+
+        children = {}
+
+        possibleResults = table[bestAttribute].unique()
+        for result in possibleResults:
+            tableAux = table[table[bestAttribute] == result]
+            if tableAux.shape[0] == 0:
+                return tree.Tree(table["result"].mode()[0],{})    
+            else:
+                remaining_attributes = [
+                    attribute for attribute in attributes
+                    if attribute != bestAttribute
+                ]
+                children[result] = self.getTree(
+                    remaining_attributes, tableAux, min_info_gain
+                )
+        
+        return tree.Tree(bestAttribute, children)
+
+    def fit(self, attributes, table):
+        self.tree = self.getTree(attributes, table, self.min_info_gain)
+
+    def predict(self, tree, row):
+        if not tree.children:
+            return tree.value
+        else:
+            attribute_value = row[tree.value]
+            if attribute_value in tree.children:
+                return self.predict(tree.children[attribute_value], row)
+            else:
+                # Si el valor del atributo no está en los hijos, retornar None o un valor por defecto
+                return None
