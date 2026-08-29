7ba658712b49eb782fd39296c9aa0be57123e650
agus433333 <agubope@gmail.com>
2026-08-29 05:18:26 -0300

    Implementacion de funciones de prediccion de naive Bayes, y cambio en el calculo de las probabilidades: ahora se hace todo con logaritmos para tener cuidado con la estabilidad numerica

--- Tarea1/naiveBayes/bayes.py
+++ Tarea1/naiveBayes/bayes.py
@@ -1,21 +1,25 @@
+import math
+
 import pandas as pd
 
 class M_Estimator:
     def __init__(self, m=2):
         self.m = m #hiperparametro
         self.model={} #este modelo es el diccionario que va a contener las probabilidades de la siguiente forma modelo[clase][atributo][valor]= probabilidad, siendo P(atributo=valor|clase)
-        self.prob_clases= {}
+        self.clases = [] #lista de clases que vamos a tener en el dataset
+        self.prob_clases= {} #aca se almacenan las P(clase)
+        
 
     #siendo X el dataframe con los atributos e Y la columna con los resultados
     def fit(self, X, Y):
 
         #obtenemos las clases
-        clases = Y.unique()
+        self.clases = Y.unique()
         #ya obtenemos el total para poder sacar las probabilidades de cada clase
         total_clases=len(Y)
 
-        for clase in clases:
-            self.prob_clases[clase]= len(Y[Y==clase])/total_clases #calculamos P(clase)
+        for clase in self.clases:
+            self.prob_clases[clase]= math.log(len(Y[Y==clase])/total_clases) #calculamos log(P(clase))
             self.model[clase]={}
 
             for attribute in X.columns:
@@ -26,10 +30,33 @@ class M_Estimator:
 
                 for value in possibleValues:
                     #calculamos P(atributo=valor|clase) con la formula de m-estimacion
-                    self.model[clase][attribute][value]= (len(X[(X[attribute]==value) & (Y==clase)]) + self.m*p_c)   /   (len(Y[Y==clase]) + self.m)
+                    self.model[clase][attribute][value]= math.log((len(X[(X[attribute]==value) & (Y==clase)]) + self.m*p_c)   /   (len(Y[Y==clase]) + self.m))
+
+                # Guardamos la probabilidad por defecto para valores nunca vistos en el entrenamiento (frecuencia = 0)
+                self.model[clase][attribute]["__default__"] = math.log((0 + self.m*p_c) / (len(Y[Y==clase]) + self.m))
 
 
-    #TODO: acá me queda la duda de que hacer, si devolver una tabla en la que lo unico que tenegamos que hacer sea poner los valores de la tupla
-    # y que nos de una probabilidad de cada clase (resultado del partido) posible, o si la idea es que nos den una tupla y devolvamos solo las probabilidades
-    # correspondientes a esta tupla 
-    #def predict(self,X):
+    #esta funcion va a predecir el resultado de una sola fila
+    def predictRow(self, row):
+        prob_per_class={}
+
+        for clase in self.clases:
+            prob_per_class[clase]=self.prob_clases[clase] #inicializamos con log(P(clase))
+
+            for attribute in row.index:
+                value=row[attribute]
+                # Obtenemos la probabilidad con .get(). Si 'value' no existe, usamos la probabilidad '__default__' calculada en fit
+                prob_value = self.model[clase][attribute].get(value, self.model[clase][attribute]["__default__"])
+                prob_per_class[clase] += prob_value #sumamos log(P(atributo=valor|clase))
+
+        #retornamos la clase con mayor probabilidad
+        return max(prob_per_class, key=prob_per_class.get)
+
+    def predict(self, X):
+        predictions=[]
+
+        for _, row in X.iterrows():
+            predictions.append(self.predictRow(row))
+
+        return pd.Series(predictions, index=X.index)
+        
